9815a9c13324bf5a37ba0738efc71910294d9786
Sergi Oliveres <solive@uic.es>
2020-05-25 18:46:54 +0200

    Returns mumy once despite having 2 vowels together. All tests passing

--- Mumi.go
+++ Mumi.go
@@ -12,7 +12,13 @@ func Convert(word string) string {
 			array[i] = "mumy"
 		}
 	}
-	return toString(array)
+	return toString(deleteDouplicates(array))
+}
+
+
+// Helper functions
+func toArray(word string) []string {
+	return strings.Split(word,"")
 }
 
 func has_more_than_30_per_cent_of_vowels(array []string) bool {
@@ -26,20 +32,6 @@ func has_more_than_30_per_cent_of_vowels(array []string) bool {
 	return thirty_per_cent < vowelCount
 }
 
-func toFloat(i int) float64 {
-	return float64(i)
-}
-
-
-
-func toArray(word string) []string {
-	return strings.Split(word,"")
-}
-
-func toString(array []string) string {
-	return strings.Join(array, "")
-}
-
 func isVowel(char string) bool {
 	vowels := [5]string{"a", "e", "i", "o", "u"}
 	vowelTable := make(map[string]bool)
@@ -49,3 +41,15 @@ func isVowel(char string) bool {
 	return vowelTable[char]
 }
 
+func toString(array []string) string {
+	return strings.Join(array, "")
+}
+
+func deleteDouplicates(array []string) []string {
+	for i := 1; i < len(array); i++ {
+		if array[i] == array [i-1] {
+			array[i] = ""
+		}
+	}
+	return array
+}
--- Mumi_test.go
+++ Mumi_test.go
@@ -17,7 +17,9 @@ func TestConvert(t*testing.T) {
 		{"Convierte una vocal en mumy", args{"a"}, "mumy"},
 		{"Convierte todas las vocales iguales de una palabra", args{"caca"}, "cmumycmumy"},
 		{"Convierte todas loas vocales de una palabra", args{"hola"}, "hmumylmumy"},
-		{"Devuelve la la misma palabra si no tiene un 30% de vocales", args{"chus"}, "chus"},
+		{"Devuelve la misma palabra si no tiene un 30% de vocales", args{"chus"}, "chus"},
+		{"Replace for mumy just once if there's 2 vowels together", args{"laura"}, "lmumyrmumy"},
+
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t*testing.T) {
