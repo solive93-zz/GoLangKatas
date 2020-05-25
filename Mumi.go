1e436a9137c3415a693d17d87a5e7290f45fcbd1
Sergi Oliveres <solive@uic.es>
2020-05-25 17:36:46 +0200

    Checks for percentage of vowels. All tests passing

--- Mumi.go
+++ Mumi.go
@@ -4,6 +4,9 @@ import "strings"
 
 func Convert(word string) string {
 	array := toArray(strings.ToLower(word))
+	if !has_more_than_30_per_cent_of_vowels(array){
+		return word
+	}
 	for i := 0; i < len(array); i++ {
 		if isVowel(array[i]) {
 			array[i] = "mumy"
@@ -11,6 +14,23 @@ func Convert(word string) string {
 	}
 	return toString(array)
 }
+
+func has_more_than_30_per_cent_of_vowels(array []string) bool {
+	thirty_per_cent := (len(array) * 30)/100
+	vowelCount := 0
+	for i := 0; i < len(array); i++ {
+		if isVowel(array[i]){
+			vowelCount++
+		}
+	}
+	return thirty_per_cent < vowelCount
+}
+
+func toFloat(i int) float64 {
+	return float64(i)
+}
+
+
 
 func toArray(word string) []string {
 	return strings.Split(word,"")
--- Mumi_test.go
+++ Mumi_test.go
@@ -17,6 +17,7 @@ func TestConvert(t*testing.T) {
 		{"Convierte una vocal en mumy", args{"a"}, "mumy"},
 		{"Convierte todas las vocales iguales de una palabra", args{"caca"}, "cmumycmumy"},
 		{"Convierte todas loas vocales de una palabra", args{"hola"}, "hmumylmumy"},
+		{"Devuelve la la misma palabra si no tiene un 30% de vocales", args{"chus"}, "chus"},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t*testing.T) {
